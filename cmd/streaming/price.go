3c42767a5e1ffde28e0bbbb7963d675ee0f204eb
hieuvt <51369562+hieu-vt@users.noreply.github.com>
2023-09-26 17:29:20 +0700

    hieu
    handle add price

--- cmd/streaming/price.go
+++ cmd/streaming/price.go
@@ -3,9 +3,11 @@ package streaming
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	goservice "github.com/200Lab-Education/go-sdk"
-	"log"
+	"io/ioutil"
 	"math/rand"
+	"net/http"
 	"price-streaming/common"
 	"price-streaming/plugin/nats"
 	"time"
@@ -23,174 +25,119 @@ type Message struct {
 	Data   Quote
 }
 
-func RandomSymbol() string {
-	rand.Seed(time.Now().UnixNano())
-
-	// Define the alphabet
-	alphabet := "ABC"
-
-	// Shuffle the alphabet
-	runes := []rune(alphabet)
-	rand.Shuffle(len(runes), func(i, j int) {
-		runes[i], runes[j] = runes[j], runes[i]
-	})
-
-	// Create a string with the first three characters
-	str := string(runes[:3])
-	return "ASX:" + str
+type Data struct {
+	Symbol string
 }
 
-func RandomQuote() string {
-	// Generate random numbers for the quote
-	symbol := RandomSymbol()
+type Response struct {
+	Data []Data
+}
 
-	openPrice := rand.Float64() * 100
-	closePrice := rand.Float64() * 100
-	lastPrice := rand.Float64() * 100
-	quoteVol := rand.Intn(1000000)
+type PriceResponse struct {
+	Symbol        string  `json:"symbol"`
+	TradePrice    float64 `json:"trade_price"`
+	Volume        int     `json:"volume"`
+	ChangePoint   int     `json:"change_point"`
+	ChangePercent int     `json:"change_percent"`
+	Bid           float64 `json:"bid"`
+	BidQty        int     `json:"bid_qty"`
+	Ask           float64 `json:"ask"`
+	AskQty        int     `json:"ask_qty"`
+	Open          float64 `json:"open"`
+	High          float64 `json:"high"`
+	Low           float64 `json:"low"`
+	PreClose      float64 `json:"pre_close"`
+	Close         float64 `json:"close"`
+	MarketStatus  string  `json:"market_status"`
+}
 
-	// Create the Quote data structure
-	quote := Quote{
-		OpenPrice:  openPrice,
-		ClosePrice: closePrice,
-		LastPrice:  lastPrice,
-		QuoteVol:   quoteVol,
+func GetSymbolDetail(url string) []string {
+	var listSymbol []string
+	//var responseObject Response
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		fmt.Print(err.Error())
+	}
+	req.Header.Add("Authorization", "Bearer eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiIrODQ5NjYxODcyMzIiLCJzdWIiOiIrODQ5NjYxODcyMzIiLCJkZXZpY2VfaWQiOiJhYmMiLCJ1c2VybmFtZSI6InVzZXIyaHNtIiwicGFydHlfaWQiOiIxMDAwMDIiLCJsb2dpbl90eXBlIjoiU1NPIiwidXNlcl9sb2dpbl90eXBlIjoidXNlcl9tb2JpbGUiLCJleHAiOjIwMDc5NTMzMDguOTUzLCJpYXQiOjE2OTI1OTMzMDh9.YVXuTzLYZpV6c2-9A7KuPDxb-Asjckr2blpSRcjKWdgt4Goq2-jF7blL-sPEMkcV4kpa_EgRXW-g_2zgphyzQgfuNTRcvvS89Y5P9Fv-62bmK--Nv24Fsa9WoAaapw9GEmrMl03EWoCzJLII7zGXS7sij8WudAs59rz8BN-Hs8FK_XZLXiKqvcpF6CeHt4MF_YXbG7tYm8dnAc45rFJZP6aGId5Zz0q0u0hpI0r7MP7Fq6irncaj6hfqhXSgR1qymFVjiwtZ2mPmpS50q64PL-jTUeDKeyc5XpzmCK-wYMJ_eQIT353gtmtNsMOYN2iNKqu7mIDsxi871Nn46mISSUs9fIdHkQei_3z8j5yMoLqBHtfA4kC35JYJTlwbInaKbSC3tQo9fnmVc9uf4gVSxil_ntkuvEz6AX4f4Gc1jRFwlfpckH0PqBb1vzhsAUU0QQRFxmZdijCie6hyDkd_1ZKMv4oDlV77pYWDknFeU2voi9TErWnEyU0pyLKYSshd9Nn-_xhqne-Un_njQ79aHyHe6IMIbHyZOGX8SUJRBvkRQ4Kg4T3GRQPFFu447ykh00_Ohe1EVP32Nl0i4MrPLaJiRqdd5R0ASfWhPnS752Xv3Y20pMElZ5JwjHju7AQN2jkr_hN3KcMHo1ToXouhotGbYEadp_yubh6jfujdLkc")
+	//json.Unmarshal(responseData, &responseObject)
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		fmt.Print(err.Error())
 	}
 
-	mess := Message{
-		Symbol: symbol,
-		Data:   quote,
+	defer res.Body.Close()
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		fmt.Print(err.Error())
 	}
 
-	result, _ := json.Marshal(mess)
+	var symbols Response
 
-	return string(result)
+	if err := json.Unmarshal([]byte(body), &symbols); err != nil {
+		fmt.Println("Error unmarshaling Json: ", err)
+	}
+
+	for _, v := range symbols.Data {
+		listSymbol = append(listSymbol, v.Symbol)
+	}
+
+	return listSymbol
+}
+
+func randomPriceResponse(symbol string) PriceResponse {
+	// Seed the random number generator with the current time
+	rand.Seed(time.Now().UnixNano())
+
+	return PriceResponse{
+		Symbol:        symbol,
+		TradePrice:    rand.Float64() * 100.0, // Example range, modify as needed
+		Volume:        rand.Intn(1000000),     // Example range, modify as needed
+		ChangePoint:   rand.Intn(100),         // Example range, modify as needed
+		ChangePercent: rand.Intn(100),         // Example range, modify as needed
+		Bid:           rand.Float64() * 100.0, // Example range, modify as needed
+		BidQty:        rand.Intn(10000),       // Example range, modify as needed
+		Ask:           rand.Float64() * 100.0, // Example range, modify as needed
+		AskQty:        rand.Intn(10000),       // Example range, modify as needed
+		Open:          rand.Float64() * 100.0, // Example range, modify as needed
+		High:          rand.Float64() * 100.0, // Example range, modify as needed
+		Low:           rand.Float64() * 100.0, // Example range, modify as needed
+		PreClose:      rand.Float64() * 100.0, // Example range, modify as needed
+		Close:         rand.Float64() * 100.0, // Example range, modify as needed
+		MarketStatus:  "main_session",         // Example, modify as needed
+	}
+}
+
+func sendDataWith(ctx context.Context, pb nats.NatsPubsub, listSymbol []string) {
+	go func() {
+		time.Sleep(time.Second * 2)
+
+		for {
+			time.Sleep(time.Millisecond)
+			for _, v := range listSymbol {
+				priceOfSymbol := randomPriceResponse(v)
+
+				body, _ := json.Marshal(priceOfSymbol)
+				err := pb.Publish(ctx, common.StreamingPriceChannel, string(body))
+
+				if err != nil {
+					fmt.Println("Send pubsub error")
+					return
+				}
+			}
+
+		}
+	}()
 }
 
 func StreamingPrice(sc goservice.ServiceContext) error {
 	pb := sc.MustGet(common.PluginNatsPubsub).(nats.NatsPubsub)
 	ctx := context.Background()
 
-	go func() {
-		time.Sleep(time.Second * 5)
+	const url = "https://cgsi-api.equix.app/v1/live/symbol?search=A"
 
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
+	listSymbols := GetSymbolDetail(url)
 
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
-
-	go func() {
-		time.Sleep(time.Second * 5)
-
-		for {
-			time.Sleep(time.Millisecond)
-			data := RandomQuote()
-			log.Println("Message: ", data)
-			pb.Publish(ctx, common.StreamingPriceChannel, data)
-		}
-	}()
+	sendDataWith(ctx, pb, listSymbols)
 
 	return nil
 }
